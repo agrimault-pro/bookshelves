f73e9ffa9845ae1e0beb8b29ab5edd532914fafb
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-11 09:44:35 +0100

    Firebase integration

--- bookshelves/src/app/app.component.ts
+++ bookshelves/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCzGYZAXGaXgImtPW3anNm272ff-6peyd4",
+      authDomain: "bookshelves-b12f3.firebaseapp.com",
+      databaseURL: "https://bookshelves-b12f3.firebaseio.com",
+      projectId: "bookshelves-b12f3",
+      storageBucket: "bookshelves-b12f3.appspot.com",
+      messagingSenderId: "121635151703"
+    };
+    firebase.initializeApp(config);
+  }
 }
