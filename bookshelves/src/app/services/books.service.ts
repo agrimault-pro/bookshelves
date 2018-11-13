843ab1e0ed012aab465732bc878b070632de42a8
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-13 12:59:51 +0100

    BookService implementation

--- bookshelves/src/app/services/books.service.ts
+++ bookshelves/src/app/services/books.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/Book.model';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  books: Book[] = [];
+  booksSubject: Subject<Book[]>;
+
   constructor() { }
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  //The "on" function allow to keep the list of books up-to-date even if another user add a new book
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      });
+  }
+
+  //Here we do not need to stay up-to-date, we fetch the book only "once"
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/'+id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, 
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookElt) => {
+        if(bookElt === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks(); //Replace the node /books, so the delete is done by the saveBooks function
+    this.emitBooks();
+  }
+
 }
