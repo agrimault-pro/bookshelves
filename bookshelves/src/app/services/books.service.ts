f0c620ae37d6f84b086bb5def83b3d72e5cc14d6
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-14 22:14:20 +0100

    Do single-book, book-form and book-list components

--- bookshelves/src/app/services/books.service.ts
+++ bookshelves/src/app/services/books.service.ts
@@ -9,7 +9,7 @@ import * as firebase from 'firebase';
 export class BooksService {
 
   books: Book[] = [];
-  booksSubject: Subject<Book[]>;
+  booksSubject = new Subject<Book[]>();
 
   constructor() { }
 
